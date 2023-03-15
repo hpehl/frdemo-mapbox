c9257924ded9298826188398f3b04fa1c7ca6053
Harald Pehl <harald.pehl@gmail.com>
2023-03-15 17:50:28 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,20 @@
+# MapBox API Mock
+
+Quarkus based service to mock the [MapBox Directions API v5](https://www.mapbox.com/) used by the [First Responder Demo](https://github.com/wildfly-extras/first-responder-demo).
+
+The service mocks the MapBox REST endpoint `/directions/v5/` using the profile `mapbox/driving`. It returns one of 10 payload samples:
+
+| Payload                                           | Size |
+|---------------------------------------------------|-----:|
+| [payload0.json](src/main/resources/payload0.json) | 128K |
+| [payload1.json](src/main/resources/payload1.json) | 112K |
+| [payload2.json](src/main/resources/payload2.json) | 128K |
+| [payload3.json](src/main/resources/payload3.json) | 136K |
+| [payload4.json](src/main/resources/payload4.json) | 140K |
+| [payload5.json](src/main/resources/payload5.json) | 324K |
+| [payload6.json](src/main/resources/payload6.json) | 1.1M |
+| [payload7.json](src/main/resources/payload7.json) | 604K |
+| [payload8.json](src/main/resources/payload8.json) |  28K |
+| [payload9.json](src/main/resources/payload9.json) | 460K |
+
+The service is available as container at [Quay.io](https://quay.io/repository/hpehl/frdemo-mapbox): `quay.io/hpehl/frdemo-mapbox`
---
+++ src/main/java/org/cajun/navy/map/DrivingDirectionsService.java
@@ -0,0 +1,58 @@
+package org.cajun.navy.map;
+
+import java.net.URL;
+import java.nio.charset.Charset;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.Random;
+
+import org.jboss.logging.Logger;
+
+import javax.inject.Inject;
+import javax.ws.rs.Consumes;
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.Response;
+
+import static javax.ws.rs.core.MediaType.APPLICATION_JSON;
+
+@Path("/directions/v5/mapbox/driving")
+@Consumes(APPLICATION_JSON)
+@Produces(APPLICATION_JSON)
+public class DrivingDirectionsService {
+
+    public static final int PAYLOADS = 10;
+
+    @Inject
+    Logger log;
+
+    @GET
+    @Path("{coordinates}")
+    public Response fakeDirections(@PathParam("coordinates") String coordinates) {
+        try {
+            int index = new Random().nextInt(PAYLOADS);
+            String payload = "/payload%d.json".formatted(index);
+            URL resource = getClass().getResource(payload);
+            if (resource != null) {
+                String json = Files.readString(Paths.get(resource.toURI()));
+                String bytes = humanReadableByteCount(json.getBytes(Charset.defaultCharset()).length, true);
+                log.infof("MapBox request for: %s. Choosing %s: %s", coordinates, payload, bytes);
+                return Response.ok(json).build();
+            } else {
+                return Response.status(Response.Status.NOT_FOUND).build();
+            }
+        } catch (Exception e) {
+            return Response.status(Response.Status.INTERNAL_SERVER_ERROR).build();
+        }
+    }
+
+    private String humanReadableByteCount(long bytes, boolean si) {
+        int unit = si ? 1000 : 1024;
+        if (bytes < unit) {return bytes + " B";}
+        int exp = (int) (Math.log(bytes) / Math.log(unit));
+        String pre = (si ? "kMGTPE" : "KMGTPE").charAt(exp - 1) + (si ? "" : "i");
+        return String.format("%.1f %sB", bytes / Math.pow(unit, exp), pre);
+    }
+}
